e553b444be924b50bb369a5779a6961de9ee8444
netGALAXY Studios <cvonlehe@netgalaxystudios.com>
2016-08-12 17:33:43 -0400

    V1B1 - Modified UI and AWS calls

--- Morphii/MorphiiCollectionViewCell.swift
+++ Morphii/MorphiiCollectionViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MorphiiCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var morphiiNameLabel: UILabel!
+    @IBOutlet weak var morphiiNameLabel: UIButton!
     @IBOutlet weak var morphiiView: MorphiiView!
     var morphii:Morphii?
     
@@ -18,12 +18,20 @@ class MorphiiCollectionViewCell: UICollectionViewCell {
         if let show = morphii.showName?.boolValue {
             shouldShow = show
         }
+        morphiiNameLabel.titleLabel?.numberOfLines = 2
+        morphiiNameLabel.titleLabel?.lineBreakMode = .ByWordWrapping
+        morphiiNameLabel.titleLabel?.textAlignment = .Center
+        morphiiNameLabel.contentHorizontalAlignment = .Center
+        morphiiNameLabel.contentVerticalAlignment = .Top
+        var name:String? = ""
         if shouldShow {
-            morphiiNameLabel.text = morphii.name
-        }else {
-            morphiiNameLabel.text = ""
-
+            name = morphii.name
         }
+        UIView.setAnimationsEnabled(false)
+        morphiiNameLabel.setTitle(name, forState: .Normal)
+        layoutIfNeeded()
+        UIView.setAnimationsEnabled(true)
+
         morphiiView.setUpMorphii(morphii, emoodl: morphii.emoodl?.doubleValue)
         self.morphii = morphii
     }
